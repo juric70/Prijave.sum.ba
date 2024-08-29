0c116aab62c148c3430fb660bcbe89e26ce11b77
matamajsta <maricmate30@gmail.com>
2024-08-30 01:36:50 +0200

    Testiranje, sorry

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// Mozda treba dodati
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
