d862b8f49201c981e70286778a62d2e2ef2071b1
mapokapo <leopetrovic11@gmail.com>
2025-03-28 20:36:38 +0100

    feat: pocetci novog dizajna radionica

--- Frontend/components/Radionice.vue
+++ Frontend/components/Radionice.vue
@@ -1,183 +1,181 @@
 <template>
-    <section class="forma">
-      <div class="button-container">
-      <button class="hero-button" @click="showInfo(1)">Otvorene prijave</button>
-      <button class="hero-button" @click="showInfo(2)">Zatvorene prijave</button>
-      <button class="hero-button" @click="showInfo(3)">Moje prijave</button>
+  <div class="flex flex-col">
+    <div class="mx-auto my-4 flex items-center gap-4">
+      <button
+        class="rounded-lg p-2 text-white transition-colors"
+        :class="{
+          'bg-[#D22D3A]': currentFilter === 'open',
+          'bg-transparent hover:bg-[#D22D3A88]': currentFilter !== 'open',
+        }"
+        @click="currentFilter = 'open'">
+        Otvorene
+      </button>
+      <button
+        class="rounded-lg p-2 text-white transition-colors"
+        :class="{
+          'bg-[#D22D3A]': currentFilter === 'closer',
+          'bg-transparent hover:bg-[#D22D3A88]': currentFilter !== 'closer',
+        }"
+        @click="currentFilter = 'closer'">
+        Zatvorene
+      </button>
+      <button
+        class="rounded-lg p-2 text-white transition-colors"
+        :class="{
+          'bg-[#D22D3A]': currentFilter === 'my',
+          'bg-transparent hover:bg-[#D22D3A88]': currentFilter !== 'my',
+        }"
+        @click="currentFilter = 'my'">
+        Moje
+      </button>
     </div>
-      <div class="hero-content">
-        <table border="3px">
-          <thead>
-            <th>Naziv radionice</th>
-            <th>Ime voditelja</th>
-            <th>Rok za prijave</th>
-            <th>Datum pocetka</th>
-            <th>Datum zavrsetka</th>
-            <th>Upravljanje</th>
-          </thead>
-          <tbody id="tijelo1" v-if="selectedButton === 1">
-            
-          </tbody>
-          <tbody id="tijelo1" v-if="selectedButton === 2">
-            
-          </tbody>
-          <tbody id="tijelo1" v-if="selectedButton === 3">
-            
-          </tbody>
-        </table>
+
+    <div
+      class="mx-8 flex flex-col gap-8"
+      v-if="radionice">
+      <div
+        class="flex rounded-lg bg-white py-8"
+        v-for="radionica in radionice"
+        :key="radionica.id">
+        <div
+          class="min-w-[100px] max-w-[150px] flex-1 rounded-br-md rounded-tr-md bg-gradient-to-b from-[#D22D3A] to-[#004371]"></div>
+        <div
+          class="mx-8 my-4 flex w-full flex-col gap-8 border-b-2 border-t-2 border-[#094776] pb-16 pt-4">
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Naziv radionice:</span>
+            {{ radionica.NazivRadionice }}
+          </span>
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Ime voditelja:</span>
+            {{ radionica.VoditeljRadionice }}
+          </span>
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Rok za prijavu:</span>
+            {{ radionica.PrijaveDo }}
+          </span>
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Datum pocetka:</span>
+            {{ radionica.DatumPocetka }}
+          </span>
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Datum zavrsetka:</span>
+            {{ radionica.DatumZavrsetka }}
+          </span>
+          <span class="font-bold text-[#1E1E1E]">
+            <span class="text-[#094776]">Opis:</span>
+            {{ radionica.OpisRadionice }}
+          </span>
+        </div>
       </div>
-    </section>
-  </template>
-  
-  
-  
-  <script>
-  import axios from 'axios';
-  axios.defaults.withCredentials = true;
-  
-  export default {
-    data(){
-      return {
-      selectedButton: 1,
-      user: [],
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import type Radionica from "~/lib/types/Radionica";
+import type User from "~/lib/types/User";
+axios.defaults.withCredentials = true;
+
+export default {
+  data(): {
+    user: User | null;
+    radionice: Radionica[] | null;
+    currentFilter: "open" | "closer" | "my";
+  } {
+    return {
+      currentFilter: "open",
+      radionice: null,
+      user: null,
+    };
+  },
+  mounted() {
+    this.getRadionice();
+    this.getUserData();
+  },
+
+  methods: {
+    async getUserData() {
+      try {
+        const userResponse = (await axios.get("http://localhost:8000/User/me"))
+          .data as User;
+
+        this.user = userResponse;
+      } catch (error) {
+        console.log(error);
       }
     },
-    mounted(){
-      this.getRadionice();
+    async getRadionice() {
+      try {
+        const radionice = (await axios.get("http://localhost:8000/Radionica"))
+          .data as Radionica[];
+
+        this.radionice = radionice.filter(radionica => {
+          const datumZavrsetka = new Date(radionica.DatumZavrsetka);
+          const trenutniDatum = new Date();
+          if (datumZavrsetka < trenutniDatum) {
+            return false;
+          }
+
+          return true;
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
-  
-    methods: {
-      async getRadionice(){
-        try{
-          await axios.get("http://localhost:8000/Radionica").then(res => {
-            res.data.forEach(radionica => {
-              let lokalno = new Date();
-              let vrijemeRadionice = new Date(radionica.PrijaveDo);
-              if(this.selectedButton == 1){if(vrijemeRadionice > lokalno)this.ispisiRadionicu(radionica);}
-              else if(this.selectedButton == 2){if(vrijemeRadionice < lokalno)this.ispisiRadionicu(radionica);}
-              else if(this.selectedButton == 3){if(radionica.IdKreatora == this.user.id)this.ispisiRadionicu(radionica);}
-            })
-          })
-          
-        }
-        catch(error){
+    async prijavi(id: number) {
+      navigateTo("/opisradionice?id=" + id);
+    },
+    async izbrisi(id: number) {
+      if (confirm("Jeste li sigurni da želite izbrisati ovu radionicu?")) {
+        try {
+          // Dohvat svih prijava radionice
+          const prijaveRadionice = (
+            await axios.get("http://localhost:8000/SvePrijave/" + id)
+          ).data as Record<string, any>[];
+
+          // Brisanje svih prijava radionice
+          for (const prijava of prijaveRadionice) {
+            // Dohvat svakog pojedinog podatka prijave
+            const podaciPrijave = (
+              await axios.get("http://localhost:8000/SviPodaci/" + prijava.id)
+            ).data as Record<string, any>[];
+
+            // Brisanje svakog pojedinog podatka prijave
+            for (const podatak of podaciPrijave) {
+              await axios.delete(
+                "http://localhost:8000/KorisnikPodatak/" + podatak.id
+              );
+            }
+
+            // Brisanje prijave
+            await axios.delete(
+              "http://localhost:8000/ListaPrijava/" + prijava.id
+            );
+          }
+
+          // Dohvat svih pitanja radionice
+          const pitanjaRadionice = (
+            await axios.get("http://localhost:8000/SvaPitanja/" + id)
+          ).data as Record<string, any>[];
+
+          // Brisanje svih pitanja radionice
+          for (const pitanje of pitanjaRadionice) {
+            // Brisanje pitanja
+            await axios.delete(
+              "http://localhost:8000/pitanjaRadionice/" + pitanje.id
+            );
+          }
+
+          // Brisanje radionice
+          await axios.delete("http://localhost:8000/Radionica/" + id);
+        } catch (error) {
           console.log(error);
         }
-  
-      },
-      async ispisiRadionicu(radionica){
-        document.getElementById("tijelo1").appendChild(document.createElement("tr"));
-        document.getElementById("tijelo1").lastChild.innerHTML = `<td>${radionica.NazivRadionice}</td><td>${radionica.VoditeljRadionice}</td><td>${radionica.PrijaveDo}</td><td>${radionica.DatumPocetka}</td><td>${radionica.DatumZavrsetka}</td><td><button class='Buttoni' data-v-bd5a589d>Prijavi se</button></td>`;
-        if(radionica.IdKreatora == this.user.id){document.getElementById("tijelo1").lastChild.innerHTML += "<td><button class='Buttoni' data-v-bd5a589d>X</button></td>";
-        document.getElementById("tijelo1").lastChild.lastChild.previousSibling.firstChild.addEventListener("click", () => this.prijavi(radionica.id));
-        document.getElementById("tijelo1").lastChild.lastChild.firstChild.addEventListener("click", () => this.izbrisi(radionica.id));}
-        else document.getElementById("tijelo1").lastChild.lastChild.firstChild.addEventListener("click", () => this.prijavi(radionica.id));
-      },
-      async prijavi(id){
-        navigateTo('/opisradionice?id='+id);
-      },
-      async izbrisi(id){
-        if(confirm("Jeste li sigurni da želite izbrisati ovu radionicu?")){
-        try{
-          axios.get("http://localhost:8000/SvePrijave/"+id).then(res => {
-            res.data.forEach(prijava => {
-              axios.get("http://localhost:8000/SviPodaci/"+prijava.id).then(resp => {
-                resp.data.forEach(podatak => {
-                  axios.delete("http://localhost:8000/KorisnikPodatak/"+podatak.id)
-                })
-                axios.delete("http://localhost:8000/ListaPrijava/"+prijava.id);
-              })
-            })
-          })
-          axios.get("http://localhost:8000/SvaPitanja/"+id).then(res => {
-            res.data.forEach(pitanje => {
-              axios.delete("http://localhost:8000/pitanjaRadionice/"+pitanje.id);
-            });
-            axios.delete("http://localhost:8000/Radionica/"+id);
-          });
-        }catch(error){console.log(error);}}
-      },
-      showInfo(buttonNumber) {
-        this.selectedButton = buttonNumber;
-        document.getElementById("tijelo1").innerHTML = "";
-        this.getRadionice();
       }
     },
-      
-}
-  
-  </script>
-  
-  <style scoped>
-  
-    .forma {
-      height: 100vh;
-      text-align: center;
-    }
-  
-    .hero-content {
-      max-width: 800px;
-    }
-  
-    table{
-      color:white;
-      font-size:20px;
-      background-color: #014479;
-      width: 60vw;
-      position: relative;
-      left: 20vw;
-      top: 5vw;  
-      text-align: left;
-      line-height: 4vh;
-      font-size: 2vh;
-      border-radius:12px;
-    }
-  
-    .Buttoni {
-      background-color: #101D2F;
-      border-radius: 8px;
-      padding: 10px 10px;
-      margin: 5px 5px;
-      border: none;
-      transition: all 0.3s ease;
-      color: white;
-      cursor: pointer;
-      text-align: center;
-  }
-  
-  .Buttoni:hover {
-    background-color: #080f18;
-  }
-  
-    .dugiodgovor {
-      width: auto;
-      height: auto;
-    }
-  
-    .dio {
-      padding: 20px 10px 5px 20px;
-      border-radius: 8px;
-    }
-  
-    .textarea-custom-width {
-      width: 50%;
-    } 
-  
-.hero-button {
-  background-color: rgba(16, 29, 47, 0.83);
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  margin: 10px;
-  font-size: 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  width: 25%;
-  max-width: 140px;
-}
+  },
+};
+</script>
 
-.hero-button:hover {
-  background-color: rgba(16, 28, 44, 0.9);
-}
-  </style>
+<style scoped></style>
